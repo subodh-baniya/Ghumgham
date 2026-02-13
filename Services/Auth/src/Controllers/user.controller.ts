2c8430cfc5378a9427a3932e47e72a35ef6129d4
Prabin K.C. <kcprabin2063@gmail.com>
2026-02-13 11:07:38 +0545

    resgister controller is now completed

--- Services/Auth/src/Controllers/user.controller.ts
+++ Services/Auth/src/Controllers/user.controller.ts
@@ -6,32 +6,38 @@ import { UserType } from "../Schema/user.schema.ts";
 import zod from "zod";
 
 const registerUser = asyncHandler(async (req: any, res: any) => {
-  const { Username, password } = req.body;
-  let validate: any;
   try {
-     validate = UserType.parse(req.body);
-  } catch (error) {
-    if (error instanceof zod.ZodError) {
-      return apiError(res, 400, "Validation failed", error);
+    const { Username, password } = req.body;
+    let validate: any;
+    try {
+       validate = UserType.parse(req.body);
+    } catch (error) {
+      if (error instanceof zod.ZodError) {
+        return apiError(res, 400, "Validation failed", error);
+      }
+      return apiError(res, 500, "Internal Server Error");
     }
-    return apiError(res, 500, "Internal Server Error");
+    console.log(validate);
+  
+    if (!Username || !password) {
+      return apiError(res, 400, "Username and password are required");
+    }
+  
+    const existingUser = await UserModel.findOne({ Username });
+  
+    if (existingUser) {
+      return apiError(res, 400, "Username already exists");
+    }
+  
+    const newUser = (await UserModel.create(validate)).isSelected("-password");
+  
+    return apiResponse(res, 201, true, "User registered successfully", newUser);
+  } catch (error) {
+    return apiError(res, 500, "Failed to register user", error);
   }
-  console.log(validate);
-
-  if (!Username || !password) {
-    return apiError(res, 400, "Username and password are required");
-  }
-
-  const existingUser = await UserModel.findOne({ Username });
-
-  if (existingUser) {
-    return apiError(res, 400, "Username already exists");
-  }
-
-  const newUser = await UserModel.create(validate);
-
-  return apiResponse(res, 201, true, "User registered successfully", newUser);
 });
+
+
 
 
 export { registerUser };
