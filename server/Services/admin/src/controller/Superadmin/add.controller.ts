50ea3a9c9cf44323de7fd920c03c7df71b662b49
Prabin K.C. <kcprabin2063@gmail.com>
2026-02-18 22:07:36 +0545

    Super admin works are being created needed to work in the database design schema and other stuff

--- server/Services/admin/src/controller/Superadmin/add.controller.ts
+++ server/Services/admin/src/controller/Superadmin/add.controller.ts
@@ -1,7 +1,7 @@
 import { apiError, asyncHandler, apiResponse , UserModel } from "@packages";
 
 
-const addSuperadmin = asyncHandler(async (req, res) => {
+const addSuperadmin = asyncHandler(async (req:any, res:any) => {
     const { email, password } = req.body;
 
     const existingUser = await UserModel.findOne({ email });
@@ -16,7 +16,7 @@ const addSuperadmin = asyncHandler(async (req, res) => {
     return apiResponse(res, 201, true, "Superadmin created successfully", { id: superadmin._id });
 })
 
-const addAdmin = asyncHandler(async (req, res) => {
+const addAdmin = asyncHandler(async (req:any, res:any) => {
     const { email, password } = req.body;
 
     const existingUser = await UserModel.findOne({ email });
@@ -31,7 +31,7 @@ const addAdmin = asyncHandler(async (req, res) => {
     return apiResponse(res, 201, true, "Admin created successfully", { id: superadmin._id });
 })
 
-const deleteAdmin = asyncHandler(async (req, res) => {
+const deleteAdmin = asyncHandler(async (req:any, res:any) => {
     const { id } = req.params;
 
     const admin = await UserModel.findById(id);
@@ -45,7 +45,7 @@ const deleteAdmin = asyncHandler(async (req, res) => {
     return apiResponse(res, 200, true, "Admin deleted successfully");
 })
 
-const deleteSuperadmin = asyncHandler(async (req, res) => {
+const deleteSuperadmin = asyncHandler(async (req:any, res:any) => {
     const { id } = req.params;
 
     const superadmin = await UserModel.findById(id);
@@ -59,7 +59,27 @@ const deleteSuperadmin = asyncHandler(async (req, res) => {
     return apiResponse(res, 200, true, "Superadmin deleted successfully");
 })  
 
+const deleteUser = asyncHandler(async (req:any, res:any) => {
+    const { id } = req.body;
+    
+    const user = await UserModel.findById(id);
+
+    if (!user) {
+        return apiError(res, 404, "User not found");
+    }
+
+    await UserModel.findByIdAndDelete(id);
+
+    return apiResponse(res, 200, true, "User deleted successfully");
+})
+
+const edituserdetails = asyncHandler(async (req:any, res:any) => {
+    const { id } = req.params;
+    if (!id) {
+        return apiError(res, 400, "User ID is required");
+    }
+
+})
 
 
-
-export default { addSuperadmin, addAdmin, deleteAdmin, deleteSuperadmin };   
+export default { addSuperadmin, addAdmin, deleteAdmin, deleteSuperadmin, deleteUser, edituserdetails };   
