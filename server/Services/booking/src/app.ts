48fdca9efb285ccd7204292c50d24b23c80d4797
subodh-baniya <baniyasubodh07@gmail.com>
2026-02-28 21:30:25 +0545

    basic changes in booking microservice for connection of db and other server other fixes are remaining

--- server/Services/booking/src/app.ts
+++ server/Services/booking/src/app.ts
@@ -1,4 +1,21 @@
-import express from "express"
+import express, { urlencoded } from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser";
 
 const app=express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use(express.json({
+    limit: '10mb' 
+}));
+
+app.use(urlencoded({extended:true}));
+
+app.use(cookieParser())
+
+export default app
--- server/Services/booking/src/index.ts
+++ server/Services/booking/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+import dotenv from "dotenv"
+import {connectDB} from "../../../Packages/Utils/connect.db.js"
+dotenv.config({
+    path:"./.env"
+})
+
+connectDB(process.env.DATABASE_URL!,process.env.DATABASE_NAME!).then(()=>{
+    console.log("Database connected successfully");
+}).catch(()=>{
+    console.log("Error in database connection")
+})
