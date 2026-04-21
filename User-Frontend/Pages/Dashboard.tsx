855eb71d74556ed01d41363e566cc52fbceef147
subodh-baniya <baniyasubodh07@gmail.com>
2026-04-21 13:45:40 +0545

    imports fixed

--- User-Frontend/Pages/Dashboard.tsx
+++ User-Frontend/Pages/Dashboard.tsx
@@ -1,5 +1,5 @@
-import Sidebar from "../components/Sidebar";
-import Topbar from "../components/Topbar";
+import Sidebar from "../Components/Sidebar";
+import Topbar from "../Components/Topbar";
 import { Outlet } from "react-router-dom";
 
 const Dashboard = () => {
